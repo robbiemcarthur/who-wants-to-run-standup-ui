dc4641c2461d99531e790e7a8a54efa740065176
Robbie McArthur <robbiemca123@gmail.com>
2025-03-19 19:35:38 +0000

    Refactor Play rock paper scissors into separate component

--- src/components/RockPaperScissors/RockPaperScissors.tsx
+++ src/components/RockPaperScissors/RockPaperScissors.tsx
@@ -1,11 +1,11 @@
 import React, {useEffect, useState} from "react";
-import {AnimatePresence, motion} from "framer-motion";
+import {motion} from "framer-motion";
 import {Link} from "react-router-dom";
 import {GameState} from "../../enum/GameState";
 import {GameResult} from "../../model/GameResult";
 import useStompClient from "../../hooks/useStompClient";
 import UsernameInputForm from "./UseInputForm";
-import RockPaperScissorsButtons from "./RockPaperScissorsButtons";
+import PlayRockPaperScissors from "./PlayRockPaperScissors";
 
 const RockPaperScissors: React.FC = () => {
     const [username, setUsername] = useState<string>("");
@@ -78,40 +78,7 @@ const RockPaperScissors: React.FC = () => {
             )}
 
             {gameState === GameState.PLAYING && (
-                <AnimatePresence>
-                    {/* Show player's choice */}
-                    {move && (
-                        <motion.p
-                            className="text-lg mt-4"
-                            initial={{opacity: 0}}
-                            animate={{opacity: 1}}
-                        >
-                            You chose: {move}
-                        </motion.p>
-                    )}
-
-                    {/* Show countdown if waiting for the opponent */}
-                    {countdown === null ? (
-                        <motion.div
-                            initial={{opacity: 0}}
-                            animate={{opacity: 1}}
-                            exit={{opacity: 0}}
-                            className="flex space-x-4"
-                        >
-                            <RockPaperScissorsButtons onSelect={sendMove}/>
-                        </motion.div>
-                    ) : (
-                        <motion.div
-                            key="countdown"
-                            initial={{scale: 0}}
-                            animate={{scale: 1}}
-                            exit={{opacity: 0}}
-                            className="text-6xl font-bold mt-4"
-                        >
-                            {countdown > 0 ? countdown : "Revealing!"}
-                        </motion.div>
-                    )}
-                </AnimatePresence>
+                <PlayRockPaperScissors sendMove={sendMove} move={move} countdown={countdown} />
             )}
 
             {gameState === GameState.RESULT && result && (
